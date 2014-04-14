26b3b2e59d02eda62d9bf096278e20ad9ed39737
Sameer Siruguri <siruguri@gmail.com>
2014-04-13 22:16:50 -0700

    better redirects; now we have titles

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -4,16 +4,28 @@ module Cms
 
       # id = all digits, means it's a true ID, else it's an attempted slug.
       if /^\d+$/.match(params[:id].to_s)
-        page_id = params[:id]
+        curr_pg = Page.find_by_id params[:id]
       else
-        # If there is no page with this slug, just display page 1.
-        page_id = 1 unless (page_id = Page.find_by_slug(params[:id]))
+        # If there is no page with this slug, redirect to 1
+        curr_pg = Page.find_by_slug(params[:id])
       end
 
+      if curr_pg.nil?
+        redirect_to root_path
+        return
+      end
+
+      page_id = curr_pg.id
       @page_content = {}
 
       @page_content[:mesg] = "This would be a page with ID = #{page_id}"
-      curr_pg = Page.find page_id
+
+      title_rec = curr_pg.page_attributes.where("key = 'title'")[0]
+      if title_rec.nil? # page does't have a title
+        @title = "Web Apps for Beginners"
+      else
+        @title = title_rec.value
+      end
 
       template = curr_pg.template
       template ||= 'template_1'
