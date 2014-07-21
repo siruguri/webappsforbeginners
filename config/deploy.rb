db7e8be9d178f183e251a8b04ec09269ae0e4d40
Sameer Siruguri <siruguri@gmail.com>
2014-07-20 20:46:42 -0700

    cap uses correct branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:siruguri/webappsforbeginners.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :branch, "master"
+set :branch, "sqlite_prod"
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/var/www/railsapps/railslms'
